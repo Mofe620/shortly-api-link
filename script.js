18359e90e8dfd437f3ad5904c38da261649e59f9
Mofe <moflux620@gmail.com>
2022-07-26 08:07:04 +0100

    ..fixed api functionality

--- script.js
+++ script.js
@@ -16,71 +16,105 @@ const Btn = document.querySelector('#shorten-btn');
 const userLink = document.querySelector('#input-link');
 Btn.addEventListener('click', ()=>{
     if(userLink.value) {
-
         validUrl = userLink.value;
         shortenLink(validUrl);
+    }
+    else {
+        userLink.setAttribute('class', 'error');
+        document.querySelector('#error').setAttribute('class', 'show');
     }
 })
 
 
 async function shortenLink(validUrl) {
-    let apiCall = await fetch(`https://api.shrtco.de/v2/shorten?url=${validUrl}`);
-    let response = await apiCall.json();
-    let shortLink1 = response.result.short_link;
-    let shortLink2 = response.result.full_short_link;
-    let shortLink3 = response.result.short_link2;
-    let shortLink4 = response.result.full_short_link2;
+    try {
+        let apiCall = await fetch(`https://api.shrtco.de/v2/shorten?url=${validUrl}`);
+        let response = await apiCall.json();
 
-    let Links = [shortLink1, shortLink2, shortLink3, shortLink4];
-    // if(JSON.parse(localStorage.getItem('SavedLinks'))){
-    //     localStorage.removeItem("SavedLinks"); 
-    // }
-    localStorage.setItem('SavedLinks', JSON.stringify(Links))
-    displayLink(Links);
+        let originalLink = response.result.original_link;
+        let shortLink1 = response.result.short_link;
+        let shortLink2 = response.result.full_short_link;
+        let shortLink3 = response.result.short_link2;
+        let shortLink4 = response.result.full_short_link2;
+
+        let Links = [originalLink, shortLink1, shortLink2, shortLink3, shortLink4];
+        
+        localStorage.setItem('SavedLinks', JSON.stringify(Links))
+        displayLink(Links);
+
+        const userLink = document.querySelector('#input-link');
+        userLink.setAttribute('class', '');
+        document.querySelector('#error').setAttribute('class', '');
+    } 
+    catch (error) {
+        const userLink = document.querySelector('#input-link');
+        userLink.setAttribute('class', 'error');
+        document.querySelector('#error').setAttribute('class', 'show');
+        document.querySelector('#error').textContent = "That is not a valid link address.."
+    }
+    
 }
 
 const resultArea = document.querySelector('.short-link-results');
 function displayLink (Links){
     resultArea.style.opacity = 1;
-    Links.map(shortLink => {
+    // if you want to display a new field for each of the shortened link variants....
+    // Links.map(shortLink => { 
         let field = document.createElement('div');
         field.setAttribute('class', 'link-field');
-        let linkText = document.createElement('p');
-        let icon = document.createElement('i');
-        linkText.textContent = shortLink;
-        icon.setAttribute('class', 'fa-regular fa-clipboard');
-        linkText.setAttribute('name', 'link');
-        field.appendChild(linkText);
-        field.appendChild(icon);
+
+        let originalLinkText = document.createElement('p');
+        originalLinkText.setAttribute('name', 'link');
+        originalLinkText.textContent = Links[0];
+
+        let newLinkArea = document.createElement('div');
+        newLinkArea.setAttribute('class', 'result');
+
+        let newLink = document.createElement('a');
+        newLink.setAttribute('href', Links[4]);
+        newLink.textContent = Links[4];
+
+        let copyBtn = document.createElement('button');
+        copyBtn.textContent = 'Copy';
+        copyBtn.setAttribute('class', 'default');
+
+        newLinkArea.appendChild(newLink);
+        newLinkArea.appendChild(copyBtn);
+        field.appendChild(originalLinkText);
+        field.appendChild(newLinkArea);
         resultArea.appendChild(field);
 
-    })
-    let clickField = document.querySelectorAll('.link-field')
-    clickField.forEach(linkArea => linkArea.addEventListener('click', (e) => copyLink(e)))
+    //})
+    let click = document.querySelectorAll('.link-field button');
+    click.forEach(button => {button.addEventListener('click', (e) => copyLink(e))});
 }
 
 
 
 function copyLink(e){
-    var clicked = e.target;
-    var child = clicked.firstChild;
+    var clickedBtn = e.target;
+    var resultDiv = clickedBtn.parentElement;
+    var link = resultDiv.firstChild;
     var range = document.createRange();
-    range.selectNode(child);
+    range.selectNode(link);
     window.getSelection().removeAllRanges(); // clear current selection
     window.getSelection().addRange(range); // to select text
     document.execCommand("copy");
     window.getSelection().removeAllRanges();// to deselect
-    if(clicked.querySelector('.fa-clipboard')){
-        clicked.querySelector('.fa-clipboard').remove();
+
+    clickedBtn.classList.toggle('copied');
+
+    let classname = clickedBtn.getAttribute('class');
+
+    if(classname.includes('copied')){
+        clickedBtn.textContent = "Copied!";
     }
-    var msg = document.createElement('span');
-    msg.textContent = 'copied to clipboard';
-    clicked.appendChild(msg);
+    else {clickedBtn.textContent = 'Copy';}
 }
 
-// Persist links in local storage
-// if(JSON.parse(localStorage.getItem('SavedLinks'))){
-//     SavedLinks = JSON.parse(localStorage.getItem('SavedLinks'));
-//     resultArea.style.opacity = 1;
-//     displayLink(SavedLinks);
-// }
+//Persist links in local storage
+if(JSON.parse(localStorage.getItem('SavedLinks'))){
+    SavedLinks = JSON.parse(localStorage.getItem('SavedLinks'));
+    resultArea.style.opacity = 1;
+    displayLink(SavedLinks);
+}
